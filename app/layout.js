5013f06cf45bb3a77a19543e5aa4631af4d2ce0e
asdfghjkl <paulinemaepatalagsa@gmail.com>
2025-06-01 18:20:15 +0800

    update title again

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ exports.metadata = void 0;
 exports.default = RootLayout;
 require("./globals.css");
 exports.metadata = {
-    title: 'Kalendryo',
+    title: 'Kalendaryo',
     description: '',
     generator: '',
 };
